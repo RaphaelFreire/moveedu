62563cddcf76485e77b09d1e0d32754b5bd0b307
RaphaelFreire <raphaelmachadofreire@gmail.com>
2019-05-30 09:21:46 -0300

    service work ajustado de forma certa

--- sw.js
+++ sw.js
@@ -15,8 +15,7 @@ let filesToCache = [
 	"/img/move-edu/bg-pattern-moveedu.svg",
 	"/js/bootstrap.min.js",
 	"/js/jquery.min.js",
-	"/js/popper.min.js",
-	"sw.js"
+	"/js/popper.min.js"
 ];
 
 // Cache on install
