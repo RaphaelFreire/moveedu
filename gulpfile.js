5e110e96ff03bfd66ed1963d59cd7f356351d9c0
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-05-29 22:36:52 -0300

    ajustes nos arquivos sass e também alterado icone do menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
-gulp.task('sass', gulp.series(function(){
-    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'scss/*.scss'])
-    .pipe(sass())
-    .pipe(gulp.dest('css'));
-}));
+gulp.task(
+	"sass",
+	gulp.series(function() {
+		return gulp
+			.src([
+				"node_modules/bootstrap/scss/*.scss",
+				"scss/*.scss",
+				"css/*.scss"
+			])
+			.pipe(sass())
+			.pipe(gulp.dest("css"));
+	})
+);
 
-gulp.task('watch', gulp.series(function(){
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'scss/*.scss'], gulp.parallel(['sass']));
-}));
+gulp.task(
+	"watch",
+	gulp.series(function() {
+		gulp.watch(
+			[
+				"node_modules/bootstrap/scss/*.scss",
+				"scss/*.scss",
+				"css/*.scss",
+				"css/*.scss"
+			],
+			gulp.parallel(["sass"])
+		);
+	})
+);
 
-gulp.task('default', gulp.series(['sass', 'watch']));
+gulp.task("default", gulp.series(["sass", "watch"]));
