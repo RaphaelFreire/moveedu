c9fbb2679f081d7bc0ab11460daf8e4c2ca3e555
RaphaelFreire <raphaelmachadofreire@gmail.com>
2019-06-04 13:12:03 -0300

    tentando fazer push simples

--- sw.js
+++ sw.js
@@ -58,3 +58,18 @@ this.addEventListener("fetch", event => {
 			})
 	);
 });
+
+
+function showNotification() {
+	Notification.requestPermission(function(result) {
+	  if (result === 'granted') {
+		navigator.serviceWorker.ready.then(function(registration) {
+		  registration.showNotification('Vibration Sample', {
+			body: 'Buzz! Buzz!',
+			vibrate: [200, 100, 200, 100, 200, 100, 200],
+			tag: 'vibration-sample'
+		  });
+		});
+	  }
+	});
+  }
