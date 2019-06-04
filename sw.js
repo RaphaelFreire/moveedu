dae13ab63f72fbe4a4a89dcdbec485aab85c7026
RaphaelFreire <raphaelmachadofreire@gmail.com>
2019-06-04 13:38:28 -0300

    tentando fazer push simples

--- sw.js
+++ sw.js
@@ -59,17 +59,27 @@ this.addEventListener("fetch", event => {
 	);
 });
 
-
-function showNotification() {
-	Notification.requestPermission(function(result) {
-	  if (result === 'granted') {
-		navigator.serviceWorker.ready.then(function(registration) {
-		  registration.showNotification('Vibration Sample', {
-			body: 'Buzz! Buzz!',
-			vibrate: [200, 100, 200, 100, 200, 100, 200],
-			tag: 'vibration-sample'
-		  });
-		});
+this.addEventListener('push', function(event) {
+	console.log('Received a push message', event);
+  
+	var title = 'Yay a message.';
+	var body = 'We have received a push message.';
+	var icon = '/img/icone-moveedu-192.png';
+	var tag = 'simple-push-demo-notification-tag';
+	var data = {
+	  doge: {
+		  wow: 'such amaze notification data'
 	  }
-	});
-  }
+	};
+  
+	event.waitUntil(
+	  self.registration.showNotification(title, {
+		body: body,
+		icon: icon,
+		tag: tag,
+		data: data
+	  })
+	);
+  });
+
+  
