810e04550faf193ff03d4245a7159d06bdb38692
RaphaelFreire <raphaelmachadofreire@gmail.com>
2019-08-02 12:16:05 -0300

    ajsute sw

--- sw.js
+++ sw.js
@@ -1,29 +1,24 @@
-// sw.js
-const staticCacheName = "ensina-01/08/2019";
+//sw.js
+const staticCacheName = "ensina-02/08/2019";
 
 let filesToCache = [
     "index.html",
-    "/img/banner-principal.png",
-    "/img/favicon.png",
-    "/img/perso-01.png",
-    "/img/perso-02.png",
-    "/img/perso-03.png",
-    "/img/perso-04.png",
-    "/img/perso-04.png",
-    "/img/detalhe-depoimento-01.svg",
-    "/img/icones-inf.svg",
-    "/img/icones-recursos.svg",
-    "/img/foto-depoimento.png",
-    "/img/assinatura-mauricio-01.svg",
-    "/img/detalhe-onda.svg",
-    "/img/icones-sup.svg",
-    "/img/ensina-mais.svg",
-    "/img/logo-moveedu.svg",
-    "/img/img-trans.svg",
-    "/js/aos.js",
-    "/js/bootstrap.min.js",
-	"/js/jquery.min.js",
-	"/js/popper.min.js"
+    "img/banner-principal.png",
+    "img/favicon.png",
+    "img/detalhe-depoimento-01.svg",
+    "img/icones-inf.svg",
+    "img/icones-recursos.svg",
+    "img/foto-depoimento.png",
+    "img/assinatura-mauricio-01.svg",
+    "img/detalhe-onda.svg",
+    "img/icones-sup.svg",
+    "img/ensina-mais.svg",
+    "img/logo-moveedu.svg",
+    "img/img-trans.svg",
+    "img/perso-01.png",
+    "img/perso-02.png",
+    "img/perso-03.png",
+    "img/perso-04.png"
 ];
 
 // Cache on install
